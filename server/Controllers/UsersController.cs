c2d1845cf5b4ce05fe7e83050c53a75acf9082cb
Roman Veselov <81238069+VeselovRoman@users.noreply.github.com>
2024-06-19 09:59:05 +0300

    Created DB and first API

---
+++ server/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Entities;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
